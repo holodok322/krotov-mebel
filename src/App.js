4c777d363c7bf638fb397a1561c937885ced7f58
holodok322 <rasultitkin2@gmail.com>
2025-03-23 06:53:54 +0300

    Обновление приложения 3

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import ModelsPanel from './Elements/ModelsPanel';
 import Footer from './Elements/Footer';
 import InfoPanel from './Elements/InfoPanel';
 import './assets/fonts/fonts.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Кротов мебель</title>
+        <meta name="description" content="Купить стулья ИЗО недорого в Свердловской области" />
+        <meta name="keywords" content="кротов мебель, купить стулья велюр, стулья велюр для кухни, купить стулья релакс, купить стулья чили, свердловская область, с доставкой, оптом" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://krotov-mebel.ru" />
+      </Helmet>
       <GeneralPanel />
       <InfoPanel />
       <MachineToolsPanel />
